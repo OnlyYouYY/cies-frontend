65647b5e0f74a2c68f956290f7c0bbdfc4e4cf27
OnlyYouYY <gtr010yam@gmail.com>
2023-05-30 12:03:10 -0400

    Api clever

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = 'https://app-84d299d1-f2c1-4453-b186-40061aa20a53.cleverapps.io/api';
-const BASE_URL = 'http://localhost:4000/api';
+const BASE_URL = 'https://app-84d299d1-f2c1-4453-b186-40061aa20a53.cleverapps.io/api';
+// const BASE_URL = 'http://localhost:4000/api';
 
 export async function login(username, password) {
   try {

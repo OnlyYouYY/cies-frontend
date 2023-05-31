b2426a61044c0e37658926acd324762f0c6005c9
OnlyYouYY <gtr010yam@gmail.com>
2023-05-30 20:27:06 -0400

    Restauracion

--- layout/AppMenu.js
+++ layout/AppMenu.js
@@ -39,20 +39,20 @@ const AppMenu = () => {
                     label: 'Gestión de categorias para servicios',
                     icon: 'pi pi-fw pi-table',
                     items: [
-                        { label: 'Nueva categoria', icon: 'pi pi-fw pi-plus', to: '/servicios/categoriaServicios' },
-                        { label: 'Actualizar categoria', icon: 'pi pi-fw pi-pencil', to: '/servicios/actualizarCategoria'},
+                        { label: 'Nueva categoria', icon: 'pi pi-fw pi-plus' },
+                        { label: 'Actualizar categoria', icon: 'pi pi-fw pi-pencil'},
                     ]
                 },
                 {
                     label: 'Gestión de servicios',
                     icon: 'pi pi-fw pi-briefcase',
                     items: [
-                        { label: 'Nuevo servicio', icon: 'pi pi-fw pi-plus', to: '/servicios/nuevoServicio' },
-                        { label: 'Actualizar servicio', icon: 'pi pi-fw pi-pencil', to: '/servicios' },
-                        { label: 'Listar servicios', icon: 'pi pi-fw pi-list', to: '/servicios/listarCategorias' }
+                        { label: 'Nuevo servicio', icon: 'pi pi-fw pi-plus'},
+                        { label: 'Actualizar servicio', icon: 'pi pi-fw pi-pencil'},
+                        { label: 'Listar servicios', icon: 'pi pi-fw pi-list'}
                     ]
                 },
-                { label: 'Citas', icon: 'pi pi-fw pi-calendar' ,to:'/servicios/citas/nuevaCita'},
+                { label: 'Citas', icon: 'pi pi-fw pi-calendar'},
             ]
 
         },
